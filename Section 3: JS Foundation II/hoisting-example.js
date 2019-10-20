ac943ba8bba60f2faa8cc7932b610cf819822d22
adam-kuhn <adam.kuhn20@gmail.com>
2019-10-20 11:22:22 +0300

    hoisting examples

---
+++ Section 3: JS Foundation II/hoisting-example.js
@@ -0,0 +1,24 @@
+console.log(letVar) // Reference Error: letVar is not defined
+console.log(constVar) // Reference Error: constVar is not defined
+console.log(varVar) // undefined
+
+let letVar = 1
+const constVar = 3
+var varVar = 'var'
+console.log('-------')
+console.log(letVar) // 1
+console.log(constVar) // 3
+console.log(varVar) // 'var'
+
+console.log(arrowFunc) // Reference Error: arrowFunc is not defined
+const arrowFunc = () => {
+  console.log('arrow')
+}
+arrowFunc() // arrow
+
+console.log(varFunc()) // varFunc is not a function
+console.log(varFunc) // undefined
+var varFunc = () => {
+  console.log('var funky')
+}
+varFunc() // var funky
