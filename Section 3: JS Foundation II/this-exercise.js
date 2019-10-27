8695dbb7858a49755a7a5ccdf771eedfe7602e36
adam-kuhn <adam.kuhn20@gmail.com>
2019-10-27 17:52:47 +0200

    mend

--- Section 3: JS Foundation II/this-exercise.js
+++ Section 3: JS Foundation II/this-exercise.js
@@ -1,12 +1,14 @@
 // what will be logged?
 
 const a = {
+  name: 'objA',
   say () {
     console.log(this)
   }
 }
 
 const b = {
+  name: 'objB',
   say () {
     return function () {
       console.log(this)
@@ -15,12 +17,13 @@ const b = {
 }
 
 const c = {
+  name: 'objC',
   say () {
     return () => {
       console.log(this)
     }
   }
 }
-a.say() // this is the a obj
-b.say()() // this is the window (function is called inside of a function not on the object, hence the ()())
-c.say()() // this is the c obj, because arrow functino forces lexical scope of this
+a.say()
+b.say()()
+c.say()()
