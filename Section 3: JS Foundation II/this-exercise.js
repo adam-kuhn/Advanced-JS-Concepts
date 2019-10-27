7d1a38b3624e3a6a021207d10c7f8b7be3e6133a
adam-kuhn <adam.kuhn20@gmail.com>
2019-10-27 17:51:17 +0200

    mend

--- Section 3: JS Foundation II/this-exercise.js
+++ Section 3: JS Foundation II/this-exercise.js
@@ -21,6 +21,6 @@ const c = {
     }
   }
 }
-a.say()
-b.say()()
-c.say()()
+a.say() // this is the a obj
+b.say()() // this is the window (function is called inside of a function not on the object, hence the ()())
+c.say()() // this is the c obj, because arrow functino forces lexical scope of this
