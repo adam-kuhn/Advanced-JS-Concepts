84470de3409081bce3811f55bc8c171b2a5139ae
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-01 11:11:40 +0200

    closuer and async function

--- Section 5: Closure and Prototypal Inheritance/closure.js
+++ Section 5: Closure and Prototypal Inheritance/closure.js
@@ -17,3 +17,15 @@ console.log(last()) // 'grandpa dad kid'
 // even though the first functions have been called and been popped off the stack because they returned
 // the variables are still referenced by lower functions and are accessible to them
 // only reference variables are kept around, the notKept variable above will be garbage collected
+
+const delayedFunction = () => {
+  setTimeout(() => {
+    console.log(text)
+  }, 5000)
+  const text = 'this works'
+}
+// becuase of clouser, and setTimeout being async, we are able to define "text"
+// after we call setTimeout, because text will be defined by the time the function inside setTimeout will run
+// and closure provides access to this variable
+
+delayedFunction()
