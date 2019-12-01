015adbc1e35f5c7b6a5e6b7e1b3b11adaa0cb2d3
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-01 12:23:14 +0200

    closure exercise 4

--- Section 5: Closure and Prototypal Inheritance/closure.js
+++ Section 5: Closure and Prototypal Inheritance/closure.js
@@ -31,7 +31,7 @@ const delayedFunction = () => {
 delayedFunction()
 
 // MEMORY EFFICIENCY
-const heavyDuty= (index) => {
+const heavyDuty = (index) => {
   const bigArray = new Array(6000).fill('this is big')
   console.log('created a massive array')
   return bigArray[index]
@@ -49,3 +49,58 @@ console.log(findItemInBigArray(5893))
 console.log(findItemInBigArray(2))
 console.log(findItemInBigArray(244))
 // with our heavyDutyEfficient function we can use closure to create the big array only once
+
+// CLOSURE EXERCISE
+// goal is only set the view once with closure
+let view
+function initialize () {
+  view = 'view is set'
+  console.log('created view')
+  // return { view }
+}
+initialize()
+initialize()
+initialize()
+
+// solution
+let viewS
+function initializeSolution () {
+  let callCount = 0
+  return () => {
+    if (callCount === 0) {
+      viewS = 'solution'
+      callCount++
+      console.log('solution set')
+    }
+  }
+}
+const startOnce = initializeSolution()
+startOnce()
+startOnce()
+startOnce()
+
+// Exercise
+
+const array = [1, 2, 3, 4]
+
+for (var i = 0; i < array.length; i++) {
+  setTimeout(function () {
+    console.log('index ' + i) // can't do array[i] because var will be equal it 4 at the end of the last loop, and array[4] is undefined
+  }, 3000)
+}
+// output is item 4, item 4, item 4, item 4
+// challenge is to log correct item -> simple solution is to change var to let,
+// let creates block scoped variables which works
+// but use var and closure to create a solution
+
+// solution
+
+// wrap setTimeout in a function, that is invoked with the current index
+for (var j = 0; j < array.length; j++) {
+  var item = (index) => {
+    setTimeout(function () {
+      console.log('item solution at ' + index)
+    }, 3000)
+  }
+  item(j)
+}
