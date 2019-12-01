3be473d0fb603a996a4a90c5062def9a8df658b6
adam-kuhn <adam.kuhn20@gmail.com>
2019-12-01 10:35:21 +0200

    higher order functions examples

---
+++ Section 5: Closure and Prototypal Inheritance/higher-order-func-exercise.js
@@ -0,0 +1,20 @@
+const multiplyBy = (num1) => (num2) => num1 * num2
+// same as above just not one lined
+const multiplyByA = function (num1) {
+  // multipleBy is a function that returns a function
+  // the function that is returned is what does the calculation
+  return function (num2) {
+    return num1 * num2
+  }
+}
+
+const multiplyByTwo = multiplyBy(2) // multiplyBy returns a function
+// therefore multiplyByTwo is equal to function (num2) {return 2 * num2} at this point and needs to be invoked to run the multiplcation
+// example of multiplyByTwo current value
+// const multiplyBytwo = function (num2) {
+//   return 2 * num2 // the 2 has been assigned from the generic multiplyBy function
+// }
+const multipleBySix = multiplyBy(6)
+console.log(multiplyByTwo(4)) // 8
+console.log(multipleBySix(4)) // 24
+console.log(multiplyBy(3)(10)) //30, multiplyBy returns a function so can directly call it again
