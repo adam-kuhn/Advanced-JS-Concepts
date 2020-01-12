b59956d4af356feb40d11d8fce478acf33f1d75f
adam-kuhn <adam.kuhn20@gmail.com>
2020-01-12 15:10:31 +0200

    OOP exercise and solution

---
+++ Section 6: Object Oriented Programming/OOP-exercise.js
@@ -0,0 +1,41 @@
+// THE PROBLEM
+// class Character {
+//   constructor(name, weapon) {
+//     this.name = name;
+//     this.weapon = weapon;
+//   }
+//   attack() {
+//     return 'atack with ' + this.weapon
+//   }
+// }
+// //Polymorphism--
+// //Extend the Character class to have a Queen class. The output of the below code should be:
+// const victoria = new Queen('Victoria', 'army', 'hearts'); // create a new instace with the queen
+// // having (name, weapon, type). Type inlcudes: 'hearts', 'clubs', 'spades', 'diamonds'
+
+// victoria.attack() // will console.log the attack() method in Character class AND will return another
+// // string: 'I am the Victoria of hearts, now bow down to me! '
+
+// SOLUTION
+class Character {
+  constructor (name, weapon) {
+    this.name = name
+    this.weapon = weapon
+  }
+  attack () {
+    return 'attack with ' + this.weapon
+  }
+}
+class Queen extends Character {
+  constructor (name, weapon, type) {
+    super(name, weapon)
+    this.type = type
+  }
+  attack () {
+    const originalAttack = super.attack() // this calls the original attack on the Character class
+    return `${originalAttack} \n I am ${this.name} the Queen of ${this.type}`
+  }
+}
+const victoria = new Queen('Victoria', 'army', 'hearts')
+
+console.log(victoria.attack())
