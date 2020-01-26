48140b959038e1f9616bdfba65256b775acfd595
adam-kuhn <adam.kuhn20@gmail.com>
2020-01-26 18:34:25 +0200

    example of Promises having a higher priority than call backs

--- Section 9: Async JS/async-intro.js
+++ Section 9: Async JS/async-intro.js
@@ -1,4 +1,9 @@
-setTimeout(() => console.log('I was written first'), 0) // prints 3rd
+setTimeout(() => console.log('I was written first'), 0) // prints 4th
+Promise.resolve('Job QUEUE!').then(data => console.log(data)) // prints 2nd
 setTimeout(() => console.log('what I am last'), 10) // prints last
-Promise.resolve('hehe').then(data => console.log(data)) // prints 2nd
+Promise.resolve('hehe').then(data => console.log(data)) // prints 3rd
 console.log('what up') // prints first
+
+// with the addition of Promises to ECMA script, came the Job Queue
+// Promises are put to the Job Queue, which has a higher priority then the
+// callback queue
